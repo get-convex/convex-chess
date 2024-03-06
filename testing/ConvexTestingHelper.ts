91da1cae9abd3e349c627b2f0d4c8715bd2321a0
Sarah Shader <sarahshader@gmail.com>
2024-03-06 14:21:31 -0500

    Try the same tests but with a JS fake

--- testing/ConvexTestingHelper.ts
+++ testing/ConvexTestingHelper.ts
@@ -1,103 +1,431 @@
-import { ConvexClient } from "convex/browser";
-import {
-  FunctionArgs,
-  FunctionReference,
-  FunctionReturnType,
-  UserIdentity,
-} from "convex/server";
-import fs from "fs";
-import path from "path";
+import { GenericId, JSONValue, Value, convexToJson, jsonToConvex } from "convex/values"
+import { GenericDocument, queryGeneric, mutationGeneric, GenericDataModel, UserIdentity, QueryBuilder, RegisteredQuery, FunctionVisibility, DefaultFunctionArgs, RegisteredMutation, GenericMutationCtx, SchemaDefinition, GenericSchema, DataModelFromSchemaDefinition } from "convex/server"
 
-/**
- * This is a helper for testing Convex functions against a locally running backend.
- *
- * An example of calling a function:
- * ```
- * const t = new ConvexTestingHelper();
- * const result = await t.query(api.foo.bar, { arg1: "baz" })
- * ```
- *
- * An example of calling a function with auth:
- * ```
- * const t = new ConvexTestingHelper();
- * const identityA = t.newIdentity({ name: "Person A"})
- * const result = await t.withIdentity(identityA).query(api.users.getProfile);
- * ```
- */
-export class ConvexTestingHelper {
-  private _nextSubjectId: number = 0;
-  public client: ConvexClient;
-  private _adminKey: string;
+/*
+- Arg validation
+- Schema validation
+- Transactions
+- Real ID algorithm
+- Actions 
+  - We need to run a function based on UDF path
+- Pagination
 
-  constructor(options: { adminKey?: string; backendUrl?: string } = {}) {
-    this.client = new ConvexClient(
-      options.backendUrl ?? "http://127.0.0.1:8000"
-    );
-    this._adminKey =
-      options.adminKey ??
-      fs.readFileSync(path.resolve(__dirname, "admin_key.txt"), "utf-8");
-  }
+*/
 
-  newIdentity(
-    args: Partial<Omit<UserIdentity, "tokenIdentifier">>
-  ): Omit<UserIdentity, "tokenIdentifier"> {
-    const subject = `test subject ${this._nextSubjectId}`;
-    this._nextSubjectId += 1;
-    const issuer = "test issuer";
-    return {
-      ...args,
-      subject,
-      issuer,
+type FilterJson = { $eq: [FilterJson, FilterJson] } | { $field: string } | JSONValue
+
+type QueryOperator = { filter: FilterJson } | { limit: number };
+type Source =
+  | { type: "FullTableScan"; tableName: string; order: "asc" | "desc" | null }
+  | {
+      type: "IndexRange";
+      indexName: string;
+      range: ReadonlyArray<SerializedRangeExpression>;
+      order: "asc" | "desc" | null;
+    }
+  | {
+      type: "Search";
+      indexName: string;
+      filters: ReadonlyArray<SerializedSearchFilter>;
     };
-  }
 
-  withIdentity(
-    identity: Omit<UserIdentity, "tokenIdentifier">
-  ): Pick<ConvexClient, "mutation" | "action" | "query"> {
-    return {
-      mutation: (functionReference, args) => {
-        (this.client as any).setAdminAuth(this._adminKey, identity);
-        return this.client.mutation(functionReference, args).finally(() => {
-          this.client.client.clearAuth();
-        });
-      },
-      action: (functionReference, args) => {
-        (this.client as any).setAdminAuth(this._adminKey, identity);
-        return this.client.action(functionReference, args).finally(() => {
-          this.client.client.clearAuth();
-        });
-      },
-      query: (functionReference, args) => {
-        (this.client as any).setAdminAuth(this._adminKey, identity);
-        return this.client.query(functionReference, args).finally(() => {
-          this.client.client.clearAuth();
-        });
-      },
+type SerializedQuery = {
+  source: Source;
+  operators: Array<QueryOperator>;
+};
+
+export type SerializedRangeExpression = {
+  type: "Eq" | "Gt" | "Gte" | "Lt" | "Lte";
+  fieldPath: string;
+  value: JSONValue;
+};
+
+export type SerializedSearchFilter =
+  | {
+      type: "Search";
+      fieldPath: string;
+      value: string;
+    }
+  | {
+      type: "Eq";
+      fieldPath: string;
+      value: JSONValue;
     };
-  }
 
-  async mutation<Mutation extends FunctionReference<"mutation">>(
-    mutation: Mutation,
-    args: FunctionArgs<Mutation>
-  ): Promise<Awaited<FunctionReturnType<Mutation>>> {
-    return this.client.mutation(mutation, args);
-  }
+class DatabaseFake {
+    private _tables: Record<string, number> = {}
+    private _documents: Record<string, { tableName: string, document: GenericDocument }> = {}
+    private _nextQueryId: number = 1;
+    private _nextDocId: number = 10000;
+    private _nextTableId: number = 10000;
+    private _queryResults: Record<string, Array<GenericDocument>> = {};
+    static __instance: null | DatabaseFake = null
+    static initialize(schema: any) {
+      if (this.__instance !== null) {
+        throw new Error("DatabaseFake initialized multiple times")
+      }
+      this.__instance = new DatabaseFake(schema)
+    }
+    static get() {  
+      if (this.__instance === null) {
+        throw new Error("DatabaseFake not initialized")
+      }
+      return this.__instance
 
-  async query<Query extends FunctionReference<"query">>(
-    query: Query,
-    args: FunctionArgs<Query>
-  ): Promise<Awaited<FunctionReturnType<Query>>> {
-    return this.client.query(query, args);
-  }
+    }
 
-  async action<Action extends FunctionReference<"action">>(
-    action: Action,
-    args: FunctionArgs<Action>
-  ): Promise<Awaited<FunctionReturnType<Action>>> {
-    return this.client.action(action, args);
-  }
+    constructor(_schema: any) {}
 
-  async close(): Promise<void> {
-    return this.client.close();
+    get(id: GenericId<string>) {
+      if (typeof id !== "string") {
+        throw new Error(
+          `Invalid argument \`id\` for \`db.get\`, expected string but got '${typeof id}': ${
+            id as any
+          }`
+        );
+      }
+
+      return this._documents[id]?.document ?? null
+    }
+
+    private _generateId<TableName extends string>(table: TableName): GenericId<TableName> {
+      const id = this._nextDocId.toString();
+      this._nextDocId += 1;
+      return id as GenericId<TableName>
+    }
+
+    insert<TableName extends string>(table: TableName, value: any) {
+      const _id = this._generateId(table)
+      const doc = {
+        ...value,
+        _id,
+        _creationTime: Date.now(),
+      }
+      this._documents[_id] = { document: doc, tableName: table };
+      // this._documentIdsByTable[table].add(_id)
+      return _id
+    }
+
+    patch<TableName extends string>(id: GenericId<TableName>, value: Record<string, any>) {
+      const result = this._documents[id];
+      if (result === undefined) {
+        throw new Error("Patch on non existent doc")
+      }
+      const { document, tableName } = result
+      this._documents[id] = { document: {...document, ...value}, tableName }
+      return
+    }
+
+    // replace<TableName extends string>(id: GenericId<TableName>, value: Record<string, any>): Promise<void> {
+    //   const doc = this._documents[id];
+    //   if (doc === undefined) {
+    //     throw new Error("Patch on non existent doc")
+    //   }
+    //   if (value._id !== undefined && value._id !== doc._id) {
+    //     throw new Error("_id mismatch")
+    //   }
+    //   this._documents[id] = {...value, _id: doc._id, _creationTime: doc._creationTime }
+    //   return Promise.resolve()
+    // }
+
+    delete(id: GenericId<string>): Promise<void> {
+      const doc = this._documents[id];
+      if (doc === undefined) {
+        throw new Error("Delete on non existent doc")
+      }
+      delete this._documents[id]
+      return Promise.resolve()
+    }
+
+    startQuery(j: JSONValue) {
+      const id = this._nextQueryId
+      const results = this._evaluateQuery((j as any).query)
+      this._queryResults[id] = results
+      this._nextQueryId += 1
+      return id
+      
+    }
+
+    queryNext(queryId: number) {
+      const results = this._queryResults[queryId];
+      if (results === undefined) {
+        throw new Error("Bad queryId")
+      }
+      if (results.length === 0) {
+        return { value: null, done: true }
+      } else {
+        return { value: results.pop(), done: false }
+      }
+
+    }
+
+    private _evaluateQuery(query: SerializedQuery): Array<GenericDocument> {
+      const source = query.source;
+      let results = []
+      let fieldPathsToSortBy = ["_creationTime"]
+      let order = "asc"
+      switch (source.type) {
+        case "FullTableScan": {
+          const tableName = source.tableName
+          for (const doc of Object.values(this._documents)) {
+            if (doc.tableName === tableName) {
+              results.push(doc.document)
+            }
+          }
+          order = source.order ?? "asc"
+          
+
+          break
+        }
+        case "IndexRange": {
+          const [tableName, indexName] = source.indexName.split(".")
+          for (const doc of Object.values(this._documents)) {
+            if (doc.tableName === tableName) {
+              results.push(doc.document)
+            }
+          }
+          results = results.filter(v => source.range.every(f => evaluateRangeFilter(v, f)))
+          // this should really look up the index
+          fieldPathsToSortBy = source.range.map(v => v.fieldPath)
+          order = source.order ?? "asc"
+          break
+        }
+        case "Search": {
+          throw new Error("not implemented")
+        }
+      }
+      const filters: Array<FilterJson> = query.operators.filter(o => (o as any).filter !== undefined).map(o => (o as any).filter)
+      // @ts-ignore
+      const limit: { limit: number } | null = query.operators.filter(o => (o as any).limit !== undefined)[0] ?? null
+    
+      results = results.filter(v => filters.every(f => evaluateFilter(v, f)))
+      
+      results.sort((a, b) => {
+        const orderMultiplier = order === "asc" ? 1 : -1
+        let v = 0;
+        for (const fp of fieldPathsToSortBy) {
+          v = compareValues(evaluateFieldPath(fp, a), evaluateFieldPath(fp, b))
+          if (v !== 0) {
+            return v * orderMultiplier
+          }
+        }
+        return v * orderMultiplier
+      })
+
+      if (limit !== null) {
+        return results.slice(0, limit.limit)
+      }
+
+      return results
+    }
+}
+
+function compareValues(a: Value | undefined, b: Value | undefined) {
+  // TODO: respect the comparison ordering between different value types
+  if (a === b) {
+    return 0
+  }
+  if (a === undefined) {
+    return -1
+  }
+  if (b === undefined) {
+    return 1
+  }
+  if (a === null) {
+    return -1
+  }
+  if (b === null) {
+    return 1
+  }
+  return a < b ? -1 : 1
+}
+
+export function isSimpleObject(value: unknown) {
+  const isObject = typeof value === "object";
+  const prototype = Object.getPrototypeOf(value);
+  const isSimple =
+    prototype === null ||
+    prototype === Object.prototype ||
+    // Objects generated from other contexts (e.g. across Node.js `vm` modules) will not satisfy the previous
+    // conditions but are still simple objects.
+    prototype?.constructor?.name === "Object";
+  return isObject && isSimple;
+}
+
+function evaluateFieldPath(fieldPath: string, document: any) {
+  const pathParts = fieldPath.split(".")
+  let result: Value | undefined = document
+  for (const p of pathParts) {
+    result = isSimpleObject(result) ? (result as any)[p] : undefined
+  }
+  return result
+}
+
+
+function evaluateFilter(document: GenericDocument, filter: any): Value | undefined {
+  if (filter.$eq !== undefined) {
+    return evaluateFilter(document, filter.$eq[0]) === evaluateFilter(document, filter.$eq[1])
+  }
+  if (filter.$field !== undefined) {
+    return evaluateFieldPath(filter.$field, document)
+  }
+  if (filter.$literal !== undefined) {
+    return filter.$literal
+  }
+  throw new Error(`not implemented: ${JSON.stringify(filter)}`)
+}
+
+function evaluateRangeFilter(document: GenericDocument, expr: SerializedRangeExpression) {
+  const result = evaluateFieldPath(expr.fieldPath, document)
+  const value = expr.value
+  switch (expr.type) {
+    case "Eq":
+      return result === value
+    case "Gt":
+      return (result as any) > (value as any)
+    case "Gte":
+      return (result as any) >= (value as any)
+    case "Lt":
+      return (result as any) < (value as any)
+    case "Lte":
+      return (result as any) <= (value as any)
+
   }
 }
+
+function syscallImpl(op: string, jsonArgs: string) {
+    const args = JSON.parse(jsonArgs);
+    switch (op) {
+      case "1.0/queryStream": {
+        const db = DatabaseFake.get();
+        const queryId = db.startQuery(args)
+        return JSON.stringify({ queryId })
+      }
+      case "1.0/queryCleanup": {
+        return JSON.stringify({})
+      }
+
+    }
+}
+
+class AuthFake {
+  private _userIdentity: any | null = null;
+  constructor() {}
+  setUserIdentity(u: any) {
+    this._userIdentity = u
+  }
+  getUserIdentity(): Promise<any> {
+    return Promise.resolve(this._userIdentity)
+  }
+}
+
+function asyncSyscallImpl(op: string, jsonArgs: string): Promise<string> {
+    const args = JSON.parse(jsonArgs);
+    switch (op) {
+        case "1.0/get": {
+            const db = DatabaseFake.get();
+            const doc = db.get(args.id);
+            return Promise.resolve(JSON.stringify(convexToJson(doc)))
+        }
+        case "1.0/insert": {
+            const db = DatabaseFake.get();
+            const _id = db.insert(args.table, jsonToConvex(args.value));
+            return Promise.resolve(JSON.stringify({ _id }))
+        }
+        case "1.0/shallowMerge": {
+          const db = DatabaseFake.get();
+          db.patch(args.id, jsonToConvex(args.value) as any);
+          return Promise.resolve(JSON.stringify({}))
+        }
+        case "1.0/queryStreamNext": {
+          const db = DatabaseFake.get();
+          const { value, done } = db.queryNext(args.queryId)
+          return Promise.resolve(JSON.stringify({ value: convexToJson(value as any), done }))
+        }
+        case "1.0/schedule": {
+          const db = DatabaseFake.get();
+          const id = db.insert("_scheduled_functions", {
+            name: args.name,
+            args: [args.args],
+            scheduledTime: args.ts,
+            state: { kind: "pending" }
+          })
+          return Promise.resolve(JSON.stringify(id))
+        }
+        default:
+          throw new Error(`Async syscall ${op} not implemented`)
+    }
+}
+
+
+export type TestConvex<SchemaDef extends SchemaDefinition<any, boolean>> = TestConvexForDataModel<DataModelFromSchemaDefinition<SchemaDef>>;
+
+export type TestConvexForDataModel<DataModel extends GenericDataModel> = {
+  auth: {
+    setUserIdentity: (identity: UserIdentity | null) => void;
+  },
+  runQuery: <Args extends DefaultFunctionArgs, Output>(func: RegisteredQuery<FunctionVisibility, Args, Output>, args: Args) => Promise<Output>;
+  runMutation: <Args extends DefaultFunctionArgs, Output>(func: RegisteredMutation<FunctionVisibility, Args, Output>, args: Args) => Promise<Output>;
+  run: <Output>(func: (ctx: GenericMutationCtx<DataModel>) => Promise<Output>) => Promise<Output>
+  cleanup: () => void;
+}
+
+export const setup = <Schema extends GenericSchema>(schema: SchemaDefinition<Schema, boolean> | null): TestConvex<SchemaDefinition<Schema, boolean>> => {
+  DatabaseFake.initialize(schema)
+  // @ts-ignore
+  const previousConvex = global.Convex;
+  // @ts-ignore
+  global.Convex = {
+    syscall: syscallImpl,
+    asyncSyscall: asyncSyscallImpl,
+  }
+  const authFake = new AuthFake();
+
+
+  const x: any = { 
+    auth: {
+      setUserIdentity: (u: any) => authFake.setUserIdentity(u)
+    },
+    runMutation: async (func: any, args: any) => {
+      const q = mutationGeneric({
+        handler: (ctx: any, a: any) => {
+          const testCtx = { ...ctx, auth: authFake }
+          return func(testCtx, a)
+        }
+      })
+      // @ts-ignore
+      const rawResult = await q.invokeMutation(JSON.stringify([args]));
+      return jsonToConvex(JSON.parse(rawResult))
+    },
+    runQuery: async (func: any, args: any) => {
+      const q = queryGeneric({
+        handler: (ctx: any, a: any) => {
+          const testCtx = { ...ctx, auth: authFake }
+          return func(testCtx, a)
+        }
+      })
+      // @ts-ignore
+      const rawResult = await q.invokeQuery(JSON.stringify([args]));
+      return jsonToConvex(JSON.parse(rawResult))
+      },
+    run: async (handler: (ctx: any) => any) => {
+      const q = mutationGeneric({
+        handler: (ctx: any) => {
+          const testCtx = { ...ctx, auth: authFake }
+          return handler(testCtx)
+        }
+      })
+      // @ts-ignore
+      const rawResult = await q.invokeMutation("[{}]");
+      return jsonToConvex(JSON.parse(rawResult))
+    },
+    cleanup: ()  => {
+      DatabaseFake.__instance = null
+      // @ts-ignore
+      global.Convex = previousConvex
+      return
+    }
+  }
+  return x
+}
+
--- tests.test.ts
+++ tests.test.ts
@@ -1,76 +1,86 @@
 import { api } from "./convex/_generated/api";
 import { ConvexError } from "convex/values";
-import { ConvexTestingHelper } from "./testing/ConvexTestingHelper";
+import { TestConvex, setup,  } from "./testing/ConvexTestingHelper";
+import schema from "./convex/schema";
+import * as games from "./convex/games";
+import { getOrCreateUser } from "./convex/users";
 
 describe("games", () => {
-  let t: ConvexTestingHelper;
+  let t: TestConvex<typeof schema>;
 
   beforeEach(() => {
-    t = new ConvexTestingHelper();
+    t = setup(schema);
   });
 
-  afterEach(async () => {
-    await t.mutation(api.testing.clearAll.default, {});
-    await t.close();
-  });
+  afterEach(() => {
+    t.cleanup()
+  })
 
   test("two players can join game", async () => {
-    const sarahIdentity = t.newIdentity({ name: "Sarah" });
-    const asSarah = t.withIdentity(sarahIdentity);
-    const gameId = await asSarah.mutation(api.games.newGame, {
+    const sarahIdentity = { subject: "test subject 1", issuer: "test issuer", name: "Sarah", tokenIdentifier: "test issuer|test subject 1"}
+    t.auth.setUserIdentity(sarahIdentity)
+    const gameId = await t.runMutation(games.newGame, {
       player1: "Me",
       player2: null,
     });
 
-    let game = await t.query(api.games.get, { id: gameId });
+    let game = await t.runQuery(games.get, { id: gameId });
     expect(game.player1Name).toEqual("Sarah");
 
-    const leeIdentity = t.newIdentity({ name: "Lee" });
-    const asLee = t.withIdentity(leeIdentity);
-    await asLee.mutation(api.games.joinGame, { id: gameId });
-    game = await t.query(api.games.get, { id: gameId });
+    const leeIdentity = { subject: "test subject 2", issuer: "test issuer", name: "Lee", tokenIdentifier: "abd|def"}
+    t.auth.setUserIdentity(leeIdentity)
+    await t.runMutation(games.joinGame, { id: gameId });
+    game = await t.runQuery(games.get, { id: gameId });
     expect(game.player2Name).toEqual("Lee");
 
-    await asSarah.mutation(api.games.move, { gameId, from: "c2", to: "c3" });
-    game = await t.query(api.games.get, { id: gameId });
+    t.auth.setUserIdentity(sarahIdentity)
+    await t.runMutation(games.move, { gameId, from: "c2", to: "c3" });
+    game = await t.runQuery(games.get, { id: gameId });
     expect(game.pgn).toEqual("1. c3");
 
     // Invalid move -- out of turn
     expect(() =>
-      asSarah.mutation(api.games.move, { gameId, from: "d2", to: "d3" })
+      t.runMutation(games.move, { gameId, from: "d2", to: "d3" })
     ).rejects.toThrow(ConvexError);
-    game = await t.query(api.games.get, { id: gameId });
+    game = await t.runQuery(games.get, { id: gameId });
     expect(game.pgn).toEqual("1. c3");
   });
 
   test("game finishes", async () => {
     // Set up data using test only functions
-    const sarahIdentity = t.newIdentity({ name: "Sarah" });
-    const asSarah = t.withIdentity(sarahIdentity);
-    const sarahId = await asSarah.mutation(api.testing.setup.user, {});
+    const sarahIdentity = { subject: "test subject 1", issuer: "test issuer", name: "Sarah", tokenIdentifier: "test issuer|test subject 1"}
+    t.auth.setUserIdentity(sarahIdentity)
+    const sarahId = await t.run((ctx) => {
+      return getOrCreateUser(ctx.db, ctx.auth);
+    });
 
-    const leeIdentity = t.newIdentity({ name: "Lee" });
-    const asLee = t.withIdentity(leeIdentity);
-    const leeId = await asLee.mutation(api.testing.setup.user, {});
+    const leeIdentity = { subject: "test subject 2", issuer: "test issuer", name: "Lee", tokenIdentifier: "abd|def"}
+    t.auth.setUserIdentity(leeIdentity)
+    const leeId = await t.run((ctx) => {
+      return getOrCreateUser(ctx.db, ctx.auth)
+    });
 
     // Two moves before the end of the game
     const gameAlmostFinishedPgn =
       "1. Nf3 Nf6 2. d4 Nc6 3. e4 Nxe4 4. Bd3 Nf6 5. Nc3 Nxd4 6. Nxd4 b6 7. O-O Bb7 8. g3 Qb8 9. Be3 c5 10. Nf5 a6 11. f3 b5 12. Bxc5 d6 13. Bd4 b4 14. Bxf6 gxf6 15. Ne2 e6 16. Nfd4 e5 17. Nf5 Qc8 18. Ne3 d5 19. Re1 d4 20. Nf1 Bh6 21. Nxd4 O-O 22. Ne2 f5 23. a3 bxa3 24. bxa3 Re8 25. Qb1 e4 26. fxe4 fxe4 27. Bxe4 Rxe4 28. Ne3 Rxe3 29. c3 Be4 30. Qb2 Bg7 31. g4 Bxc3 32. Nxc3 Rxc3 33. Rxe4 Kg7 34. g5 Kg6 35. Re7 Rc7 36. Qf6+ Kh5 37. Re5 Rb8 38. Rae1 Rc6 39. Qxf7+ Kg4 40. Qxh7 Rb7 41. Qd3 Rbc7 42. Rd1 Rc3 43. Qd4+ Kh5 44. a4 R7c4 45. g6+ Kxg6 46. Qd6+ Kf7 47. Re7+ Kf8 48. Rc7+";
 
-    const gameId = await t.mutation(api.testing.setup.game, {
-      player1: sarahId,
-      player2: leeId,
-      pgn: gameAlmostFinishedPgn,
-      finished: false,
+    const gameId = await t.run((ctx) => {
+      return ctx.db.insert("games", {
+        player1: sarahId,
+        player2: leeId,
+        pgn: gameAlmostFinishedPgn,
+        finished: false,
+      })
     });
 
     // Test that winning the game marks the game as finished
-    await asLee.mutation(api.games.move, { gameId, from: "f8", to: "e8" });
-    let game = await t.query(api.games.get, { id: gameId });
+    await t.runMutation(games.move, { gameId, from: "f8", to: "e8" });
+    let game = await t.runQuery(games.get, { id: gameId });
     expect(game.finished).toBe(false);
 
-    await asSarah.mutation(api.games.move, { gameId, from: "d6", to: "e7" });
-    game = await t.query(api.games.get, { id: gameId });
+    t.auth.setUserIdentity(sarahIdentity)
+    await t.runMutation(games.move, { gameId, from: "d6", to: "e7" });
+    game = await t.runQuery(games.get, { id: gameId });
     expect(game.finished).toBe(true);
   });
 });
