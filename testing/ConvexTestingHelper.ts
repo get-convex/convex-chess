3639c1ad0a7a51d4ee5fb56d684dfa9b6a348b89
Sarah Shader <sarahshader@gmail.com>
2024-03-06 12:12:24 -0500

    Add helper files for running against a local backend

---
+++ testing/ConvexTestingHelper.ts
@@ -0,0 +1,101 @@
+import { ConvexClient } from 'convex/browser';
+import {
+  FunctionArgs,
+  FunctionReference,
+  FunctionReturnType,
+  UserIdentity,
+} from 'convex/server';
+import fs from 'fs';
+
+/**
+ * This is a helper for testing Convex functions against a locally running backend.
+ * 
+ * An example of calling a function:
+ * ```
+ * const t = new ConvexTestingHelper();
+ * const result = await t.query(api.foo.bar, { arg1: "baz" })
+ * ```
+ * 
+ * An example of calling a function with auth:
+ * ```
+ * const t = new ConvexTestingHelper();
+ * const identityA = t.newIdentity({ name: "Person A"})
+ * const result = await t.withIdentity(identityA).query(api.users.getProfile);
+ * ```
+ */
+export class ConvexTestingHelper {
+  private _nextSubjectId: number = 0;
+  public client: ConvexClient;
+  private _adminKey: string;
+
+  constructor(options: { adminKey?: string; backendUrl?: string } = {}) {
+    this.client = new ConvexClient(
+      options.backendUrl ?? 'http://127.0.0.1:8000'
+    );
+    this._adminKey =
+      options.adminKey ?? fs.readFileSync('./admin_key.txt', 'utf-8');
+  }
+
+  newIdentity(
+    args: Partial<Omit<UserIdentity, 'tokenIdentifier'>>
+  ): Omit<UserIdentity, 'tokenIdentifier'> {
+    const subject = `test subject ${this._nextSubjectId}`;
+    this._nextSubjectId += 1;
+    const issuer = 'test issuer';
+    return {
+      ...args,
+      subject,
+      issuer,
+    };
+  }
+
+  withIdentity(
+    identity: Omit<UserIdentity, 'tokenIdentifier'>
+  ): Pick<ConvexClient, 'mutation' | 'action' | 'query'> {
+    return {
+      mutation: (functionReference, args) => {
+        (this.client as any).setAdminAuth(this._adminKey, identity);
+        return this.client.mutation(functionReference, args).finally(() => {
+          this.client.client.clearAuth();
+        });
+      },
+      action: (functionReference, args) => {
+        (this.client as any).setAdminAuth(this._adminKey, identity);
+        return this.client.action(functionReference, args).finally(() => {
+          this.client.client.clearAuth();
+        });
+      },
+      query: (functionReference, args) => {
+        (this.client as any).setAdminAuth(this._adminKey, identity);
+        return this.client.query(functionReference, args).finally(() => {
+          this.client.client.clearAuth();
+        });
+      },
+    };
+  }
+
+  async mutation<Mutation extends FunctionReference<'mutation'>>(
+    mutation: Mutation,
+    args: FunctionArgs<Mutation>
+  ): Promise<Awaited<FunctionReturnType<Mutation>>> {
+    return this.client.mutation(mutation, args);
+  }
+
+  async query<Query extends FunctionReference<'query'>>(
+    query: Query,
+    args: FunctionArgs<Query>
+  ): Promise<Awaited<FunctionReturnType<Query>>> {
+    return this.client.query(query, args);
+  }
+
+  async action<Action extends FunctionReference<'action'>>(
+    action: Action,
+    args: FunctionArgs<Action>
+  ): Promise<Awaited<FunctionReturnType<Action>>> {
+    return this.client.action(action, args);
+  }
+
+  async close(): Promise<void> {
+    return this.client.close();
+  }
+}
---
+++ testing/checkLocalBackend.js
@@ -0,0 +1,69 @@
+const http = require('http');
+const chalk = require('chalk');
+const readline = require('readline');
+const url = require('url');
+const { spawnSync } = require('child_process');
+
+const parsedUrl = url.parse('http://127.0.0.1:8000');
+http
+  .request(
+    {
+      hostname: parsedUrl.hostname,
+      port: parsedUrl.port,
+      path: '/version',
+      method: 'GET',
+    },
+    (res) => {
+      if (res.statusCode === 200) {
+        process.exit(0);
+      } else {
+        onFailure();
+      }
+    }
+  )
+  .on('error', onFailure)
+  .end();
+
+function onFailure() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.on('SIGINT', () => {
+    rl.close();
+    process.exit(1);
+  });
+
+  rl.question(
+    chalk.yellow(
+      "Looks like you don't have local backend running, start it now? [Y/n]: "
+    ),
+    (answer) => {
+      rl.close();
+      if (
+        answer.toLowerCase() === 'yes' ||
+        answer.toLowerCase() === 'y' ||
+        answer === ''
+      ) {
+        console.error(
+          chalk.green(
+            'Starting just run-local-backend now via `just run-local-backend`, ' +
+              'repeat your original command in a new terminal'
+          )
+        );
+        spawnSync('just run-local-backend', { shell: true, stdio: 'inherit' });
+        console.log(chalk.green('Quiting local backend, all is good!'));
+        process.exit(1);
+      } else {
+        console.error(
+          chalk.yellow(
+            `Make sure to run ${chalk.white.bold(
+              'just run-local-backend'
+            )} in another terminal!`
+          )
+        );
+      }
+    }
+  );
+}
