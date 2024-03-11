fa05a4dba1d4d2d8dfa4a51bc013e7afa1b4c56b
Sarah Shader <sarahshader@gmail.com>
2024-03-11 16:18:50 -0400

    Simple test running against local backend

--- games.test.ts
+++ games.test.ts
@@ -1,6 +1,19 @@
+import { ConvexTestingHelper } from "convex-helpers/testing"
+import { api } from "./convex/_generated/api";
 describe("games", () => {
+  let t: ConvexTestingHelper;
+
+  beforeEach(() => {
+    t = new ConvexTestingHelper();
+  })
+
+  afterEach(async () => {
+    await t.mutation(api.testing.clearAll, {})
+    await t.close()
+  })
 
   test("simple", async () => {
-    expect("Hello world").toStrictEqual("Hello world")
-    })
+    const games = await t.query(api.games.ongoingGames, {})
+    expect(games.length).toStrictEqual(0)
+  })
 })
