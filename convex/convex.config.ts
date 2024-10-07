b25eaf97c7b0081f9bc9d29a2e15d0ad0abdc8fc
Sujay Jayakar <sujayakar@convex.dev>
2024-10-07 18:21:19 -0400

    Remove cast

--- convex/convex.config.ts
+++ convex/convex.config.ts
@@ -2,5 +2,5 @@ import { defineApp } from "convex/server";
 import aggregate from "@convex-dev/aggregate/convex.config.js";
 
 const app = defineApp();
-app.use(aggregate as any);
+app.use(aggregate);
 export default app;
